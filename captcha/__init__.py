6ab8fbc16263aa0ea34e911f6240feaafd78924b
LepkoQQ <miha@lepko.net>
2026-04-11 13:18:59 +0200

    add dice captcha

--- captcha/__init__.py
+++ captcha/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseCaptcha
+from .dice_captcha import DiceCaptcha
 from .text_captcha import TextCaptcha
 
-__all__ = ["BaseCaptcha", "TextCaptcha"]
+__all__ = ["BaseCaptcha", "DiceCaptcha", "TextCaptcha"]
